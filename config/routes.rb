c0caebb7c72e5944388996f0158c7c2faaa945c5
Erick Saavedra <esvdrah@gmail.com>
2020-06-03 21:53:01 -0500

    Devise done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get '/my_profile', to: 'users#show'
+  root to: 'home#index'
 end
