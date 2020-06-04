dfe7b067651ef257aeeecc71b06a9a8abf8cc4f9
Erick Saavedra <esvdrah@gmail.com>
2020-06-03 20:45:11 -0500

    update migration User

--- db/migrate/20200603215023_create_users.rb
+++ db/migrate/20200603215023_create_users.rb
@@ -4,9 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :username, null: false
-      t.string :password
       t.string :name
-      t.string :email, null: false
       t.string :location
       t.string :bio
       t.integer :follow_count, default: 0
--- db/schema.rb
+++ db/schema.rb
@@ -41,9 +41,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_222631) do
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
-    t.string "password"
     t.string "name"
-    t.string "email", null: false
     t.string "location"
     t.string "bio"
     t.integer "follow_count", default: 0
