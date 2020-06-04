c651f9556f56f1beacd6d827f751f6775394ea67
Erick Saavedra <esvdrah@gmail.com>
2020-06-03 23:21:18 -0500

    Update seed, and delete string and table Users

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby '2.6.3'
 # Devise
 gem 'devise'
 
+gem 'faker'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.1'
 # Use postgresql as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -34,11 +34,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_022351) do
     t.index ["user_id"], name: "index_tuits_on_user_id"
   end
 
-  create_table "tuits_users", id: false, force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "tuit_id", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "name"
@@ -49,7 +44,6 @@ ActiveRecord::Schema.define(version: 2020_06_04_022351) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
-    t.string "string", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
