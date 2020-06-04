55a8be592446d88fb421e72dcc94e9fdb98d44db
Alessandro Chumpitaz <par_214@hotmail.com>
2020-06-04 02:27:57 -0500

    Added Some Associations and Models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :tuit
   belongs_to :user
+
+  validates :body,presence: { message: 'Meme must have a body' }, length: { in:1..280 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #Validations
+  # Tuits
   has_many :tuits
+  # Comments
+  has_many :comments
+  has_many :commented_tuits, through: :comments, source: :tuit
+  # Likes
+     # has_many :likes
+     # has_many :liked_tuits, through: likes, source: :tuit
+  # Follows
+    # TODO
+  validates :username, presence: {message: 'Username is required'}, uniqueness: {message: 'Username taken'}
+  validates :email, presence: {message: 'email is required'}, uniqueness: {message: 'Already an account have this email linked'}
 
-
-
-  ##Validations
   validates :name, presence:true
 end
